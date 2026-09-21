598a3ac91b7c2bb60ddd732a3fe58f1a5d16c11e
Taras <tarasboretsky@gmail.com>
2026-09-21 10:24:35 +0300

    lab01: solution CrossApp, Cli, вибір домену

---
+++ src/Cli/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
+Console.WriteLine("Студент: Тарас Борецький");
+Console.WriteLine(new string('-', 52));
+
+Console.WriteLine($"ОС (OSDescription) : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"ОС (Environment) : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
+Console.WriteLine($"Runtime : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку : {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог : {Environment.CurrentDirectory}");
+
+Console.WriteLine(new string('-', 52));
+
+Console.WriteLine("Предметна область: Замовлення");
+Console.WriteLine("Сутності: Customer, Product, Order, OrderLine");
+Console.WriteLine("Призначення: оформлення замовлень і підрахунок їх загальної вартості.");
